010b370b9445402f7c24c80e551eb034f4eb8360
Yeohoon <yhoon.yoon@gmail.com>
2021-12-08 16:43:03 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function cats(){
+  return cats;
+}
+function destructivelyAppendCat(name){
+  return cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+  return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+  return cats.pop(); 
+}
+function destructivelyRemoveFirstCat(){
+  return cats.shift();
+}
+function appendCat(name){
+ let newCats = [...cats, "Broom"]
+ return newCats;
+}
+function prependCat(name){
+  let newArray = ["Arnold", ...cats];
+  return newArray;
+}
+function removeLastCat(){
+  return cats.slice(0,2);
+}
+function removeFirstCat(){
+  return cats.slice(1);
+}
